efe79f51a85cc67b050015846341e6a1c2046697
Kevin Oliver <kevinoliver@gmail.com>
2023-03-19 17:44:42 -0700

    Remove out of date todo in beesolver

--- beesolver.go
+++ beesolver.go
@@ -4,7 +4,6 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello, World!")
-	// todo: maybe should be dictionary.New
 	d1 := NewDictionaryFromPath("/home/kevin/dev/beesolver-go/beesolver.go")
 	w1 := d1.Words()
 	fmt.Println(len(w1))
